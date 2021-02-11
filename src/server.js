4d54252e2d0cfb9f94eb1d51f7c5b37b7dd896b1
Melvin Suratos <melvinsuratos@hotmail.com>
2021-02-10 22:09:12 -0800

    Updated port to listen to

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const rooteRoute = require('./routes/root-route');
 const app = express();
-const port = 3001;
+const port = 443;
 
 app.get('/', (req, res) => {
     res.send('Root endpoint of Valentine Hunt Api');
@@ -11,5 +11,5 @@ app.get('/', (req, res) => {
 app.use('/api/v1', rooteRoute);
 
 app.listen(port, () => {
-    console.log(`Backend Api for Valentine Hunt is listening at http://localhost:${port}`);
+    console.log(`Backend Api for Valentine Hunt is listening at on ${port}`);
 });
