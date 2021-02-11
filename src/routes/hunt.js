0e2bc1d1ed2ddfe74d22fb229c57660de2403714
Melvin Suratos <melvinsuratos@hotmail.com>
2021-02-11 13:23:35 -0800

    Added postgres connection & get hunts endpoint

--- src/routes/hunt.js
+++ src/routes/hunt.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const db = require('../config/db');
 
 const router = express.Router();
 
-router.route('/')
-    .get((req, res) => res.send('get hunt api'));
+router.get('/', (req, res) => {
+    db.connect((err, client, done) => {
+        if (err) {
+            console.log(`Error occured in GET /hunt: ${err.name}`, err.message);
+
+            res.status(500);
+            res.statusMessage = err.message + '\n' + err.stack;
+            res.send(err.name);
+        }
+
+        client.query("SELECT * FROM public.hunts", [], (err, result) => {
+            console.log (result.rows);
+            res.json(result.rows);
+        });
+    });
+});
 
 module.exports = router;
