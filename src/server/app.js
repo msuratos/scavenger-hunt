19e7750589211e8458b2a39702a608c2aee2ba90
Melvin Suratos <melvinsuratos@hotmail.com>
2021-02-12 14:04:55 -0800

    Implemented post clue logic in the api

--- src/server/app.js
+++ src/server/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const rooteRoute = require('./routes/root-route');
 
 const path = __dirname + '/build/';
-const app = express();
 const port = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.static(path));
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
     res.sendFile(path + "index.html");
--- src/server/routes/clue.js
+++ src/server/routes/clue.js
@@ -16,6 +16,7 @@ router.get('/:huntid', (req, res) => {
 
         const huntid = req.params.huntid;
 
+        // Validate huntid
         if (huntid === undefined || huntid === '' || huntid === null) {
             res.status(404);
             res.statusMessage = 'HuntId can not be null or empty. Please enter valid HuntId';
@@ -23,7 +24,9 @@ router.get('/:huntid', (req, res) => {
             return res.send('Invalid HuntId. Please try again.');
         }
 
-        const getCluesQuery = `SELECT clues.*, hunts.hunt FROM public.clues
+        const getCluesQuery = ` SELECT clues.cluesid, clues.clues, clues.createdby, clues.createddate,
+                                clues.fk_huntid, encode(clues.image::bytea, 'base64') as image,
+                                hunts.hunt FROM public.clues
                                 INNER JOIN public.hunts ON public.clues.fk_huntid = public.hunts.huntid
                                 WHERE hunts.huntid = '${huntid}'`;
     
@@ -42,5 +45,60 @@ router.get('/:huntid', (req, res) => {
         });
     });
 });
+
+router.post('/:huntid', (req, res) => {
+    const { clues, createdby, image } = req.body;
+
+    db.connect(async (err, client, done) => {
+        if (err) {
+            console.log(err.stack);
+    
+            res.status(500);
+            res.statusMessage = `${err.name}: ${err.message}`;
+    
+            return res.send(err.stack);
+        }
+
+        const huntid = req.params.huntid;
+
+        // Validate huntid
+        if (huntid === undefined || huntid === '' || huntid === null) {
+            res.status(404);
+            res.statusMessage = 'HuntId can not be null or empty. Please enter valid HuntId';
+
+            return res.send('Invalid HuntId. Please try again.');
+        } else {
+            var hunt = await client.query('select huntid from public.hunts where huntid = $1', [huntid]);
+
+            if (!hunt) {
+                res.status(404);
+                res.statusMessage = `Invalid huntid, could not find ${huntid}`;
+        
+                return res.end();
+            }
+        }
+
+        // Insert values
+        try {
+            await client.query('BEGIN');
+            const insertClue = 'INSERT INTO public.clues(clues, createdby, createddate, fk_huntid, image) VALUES ($1, $2, CURRENT_TIMESTAMP, $3, $4)';
+            const insertClueValues = [clues, createdby, huntid, image];
+            await client.query(insertClue, insertClueValues);
+            await client.query('COMMIT');
+
+            res.sendStatus(200);
+        } catch (e) {
+            await client.query('ROLLBACK')
+            
+            res.status(500);
+            res.statusMessage = `Could not insert new clue record for hunt ${huntid}`;
+
+            res.end();
+
+        } finally {
+            client.release();
+        }
+    });
+})
 
 module.exports = router;
