1ec63971a410021c5a1615da26467cb34cd05a0c
Melvin Suratos <melvinsuratos@hotmail.com>
2021-02-12 17:30:28 -0800

    Fixed QR Url config

--- src/client/src/config/qrUrlConfig.js
+++ src/client/src/config/qrUrlConfig.js
@@ -1,5 +1,5 @@
 function getQrUrl() {
-    let url = process.env.NODE_ENV=='development' ? 'http://localhost:3001' : window.location.href;
+    let url = process.env.NODE_ENV=='development' ? 'http://localhost:3001' : (`${window.location.protocol}//${window.location.hostname}`);
     console.log('QR Base Url', url);
 
     return url;
