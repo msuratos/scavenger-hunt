fec475b81da9d896194d0621b0fc4619de88564c
Melvin Suratos <melvinsuratos@hotmail.com>
2021-02-12 17:17:34 -0800

    Fixed url request for api and qr code

--- src/client/src/config/serviceConfig.js
+++ src/client/src/config/serviceConfig.js
@@ -1,5 +1,5 @@
 function getApiUrl() {
-    let url = process.env.NODE_ENV=='development' ? 'http://localhost:3001' : '';
+    let url = process.env.NODE_ENV=='development' ? 'http://localhost:3001' : window.location.href;
 
     return url;
 }
