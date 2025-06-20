a8f5d0d483cdf6a93c8499d7161dc9cd8f3bac3a
Melvin Suratos <melvinsuratos@hotmail.com>
2025-06-19 21:46:47 -0700

    implemented basic logic when submitting player name to join hunt

--- scavengerhunt.react/src/pages/JoinHuntPage.jsx
+++ scavengerhunt.react/src/pages/JoinHuntPage.jsx
@@ -3,6 +3,7 @@ import { Button, TextInput } from '@mantine/core';
 import { useNavigate, useParams } from 'react-router';
 
 export default function JoinHuntPage() {
+  const [submitButtonLoading, setSubmitButtonLoading] = React.useState(false);
   const [code, setCode] = React.useState('');
   const [codeExists, setCodeExists] = React.useState(false);
   const [playerName, setPlayerName] = React.useState('');
@@ -14,6 +15,15 @@ export default function JoinHuntPage() {
     if (params.code) setCode(params.code);
     setCodeExists(params.code !== undefined && params.code !== '');
   }, [params.code]);
+
+  async function joinHunt() {
+    const joinDetails = { huntCode: code, playerName };
+    setSubmitButtonLoading(true);
+
+    console.debug('Joining hunt', joinDetails);
+    // TODO: call web api to join hunt with code and player details
+    // TODO: if web api call is successful, navigate to player hunt page
+  }
 
   return (
     <>
@@ -27,7 +37,7 @@ export default function JoinHuntPage() {
       {codeExists && (
         <>
           <TextInput label="Player Name" placeholder="Enter name" value={playerName} onChange={e => setPlayerName(e.currentTarget.value.trim())} withAsterisk />
-          <Button disabled={playerName.trim() === ''}>Submit</Button>
+          <Button disabled={playerName.trim() === ''} loading={submitButtonLoading} onClick={joinHunt}>Submit</Button>
         </>
       )}
     </>
