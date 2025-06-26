aab57acc6c11c33dc8845c078e48e9f957f9caf0
Melvin Suratos <melvinsuratos@hotmail.com>
2025-06-26 08:35:58 -0700

    Improve UI for hunt statuses and upload experience
    Enhanced hunt status display in HuntPage.jsx:
    - Centered text and adjusted title orders for better readability.
    - Added a smaller title for "Ended" status.
    
    Refined upload flow in PlayerItemPage.jsx:
    - Replaced progress bar with RingProgress and Loader.
    - Disabled "Back" button during uploads.
    - Hid FileInput while uploading to reduce confusion.

--- scavengerhunt.react/src/pages/HuntPage.jsx
+++ scavengerhunt.react/src/pages/HuntPage.jsx
@@ -11,11 +11,11 @@ export default function HuntPage() {
       {hunt?.status === 'Not Started' && (
         <>
           <Center>
-            <Title order={1} c='forest'>{hunt.title}</Title>
+            <Title order={1} c='forest' ta='center'>Hunt Not Started</Title>
           </Center>
 
           <Center>
-            <Title order={2} c='forest'>Not Started</Title>
+            <Title order={5} c='forest' ta='center'>{hunt.title}</Title>
           </Center>
         </>
       )}
@@ -25,11 +25,15 @@ export default function HuntPage() {
       {hunt?.status === 'Ended' && (
         <>
           <Center>
-            <Title order={1} c='forest'>{hunt.title}</Title>
+            <Title order={1} c='forest' ta='center'>Hunt Has Ended!</Title>
           </Center>
 
           <Center>
-            <Title order={2} c='forest'>Hunt has ended, thanks for playing!</Title>
+            <Title order={2} c='forest' ta='center'>Thanks for playing!</Title>
+          </Center>
+
+          <Center>
+            <Title order={5} c='forest' ta='center'>{hunt.title}</Title>
           </Center>
         </>
       )}
